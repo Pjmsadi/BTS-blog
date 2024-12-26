7bb5b45fc4364be352e70e9e6afd5696adcfc6f6
Pjmsadi <eshaalZahrah5@gmail.com>
2024-12-27 02:05:29 +0500

    Updated page

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,16 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Client } from './lib/sanity'; // Sanity client import
 import imageUrlBuilder from '@sanity/image-url'; // Sanity image URL builder
 import Image from 'next/image'; // Next.js Image component
-import { } from 'next-sanity';
 
 // Image URL builder setup
 const builder = imageUrlBuilder(Client);
 
-// Ye function Sanity image ka URL banayega
-export function url({ source }: { source: any; }) {
+// Function to generate Sanity image URL
+export function urlFor(source: any) {
   return builder.image(source);
 }
 
-// Data fetch karne ka function
+// Function to fetch data
 async function getData() {
   const query = `
     *[_type == 'blog'] | order(_createdAt desc) {
@@ -36,23 +35,23 @@ export default async function Home() {
         <Card key={idx}>
           {/* Blog Image */}
           <Image
-            src={url({ source: post.titleImage }).url()} // Generate URL
-            alt="image" // Default alt text
+            src={urlFor(post.titleImage).url()} // Generate URL
+            alt={post.title} // Use post title as alt text
             width={400} // Image width
             height={300} // Image height
-            className= "rounded-t-lg h-[200px] object-cover"
+            className="rounded-t-lg h-[200px] object-cover"
           />
-          <CardContent className= "mt-3">
-          <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-    <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-      {post.smallDescription}
-    </p>
-    <a 
-      href={`/blog/${post.currentSlug}`} 
-      className="w-full mt-7 inline-block bg-blue-500 text-white text-center py-2 px-4 rounded hover:bg-blue-600"
-    >
-      Read more
-    </a>
+          <CardContent className="mt-3">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <a 
+              href={`/blog/${post.currentSlug}`} 
+              className="w-full mt-7 inline-block bg-blue-500 text-white text-center py-2 px-4 rounded hover:bg-blue-600"
+            >
+              Read more
+            </a>
           </CardContent>
         </Card>
       ))}
